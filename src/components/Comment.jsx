c4963615c8360f367a20d205e9a0a4d386ed3c39
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-08-22 18:26:32 +0300

    Add Comment JSX

---
+++ src/components/Comment.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Comment = () => {
+  return (
+    <div className='commentSection'>
+        <p className="hash">[#]</p>
+        <p className='time'>12:24</p>
+        <p className='usernames'>Edward</p>
+        <p className='comments'>This is great!!</p>  
+    </div>
+  )
+}
+
+export default Comment
