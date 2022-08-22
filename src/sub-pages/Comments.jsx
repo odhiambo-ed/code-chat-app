913fe234da7333b458ea4a939062ac9bc5acca13
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-08-22 19:04:32 +0300

    Add Send Image

---
+++ src/sub-pages/Comments.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Comment from '../components/Comment'
+import Send from '../assets/send.jpeg'
+
+const Comments = () => {
+  return (
+      <div>
+          
+    </div>
+  )
+}
+
+export default Comments
