68d3e25bfae8082364cf32d755bc0c43c4930f1b
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-08-22 19:41:45 +0300

    Add Login JSX

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Google from '../assets/google.jpeg'
 
 const Login = () => {
   return (
-    <div>Login</div>
+      <div className="loginDiv">
+          <h2 className="appTitle">
+              Code Chat App
+          </h2>
+          <div className="signInDiv">
+              <img src={Google} alt="Google" className="signInImage" />
+              <p className="signInButton">
+                  Sign In With <span className="signInAlt">Google</span>
+              </p>
+          </div>
+    </div>
   )
 }
 
