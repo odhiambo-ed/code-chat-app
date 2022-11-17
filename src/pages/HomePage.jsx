95991f4a31786d7c1e43ce0931a9afd5840e6552
white3d <odhiambo.edward111@gmail.com>
2022-11-17 13:01:04 +0300

    Add Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
-import Body from "../sub-pages/Body";
-import Comments from "../sub-pages/Comments";
-import Followed from "../sub-pages/Followed";
+import React, { useState, useEffect } from "react";
 import "../styles/HomePage.css";
+import db from "../firebase";
 
-const HomePage = () => {
+import { Followed } from "../sub-pages/Followed";
+import { Body } from "../sub-pages/Body";
+import { Comments } from "../sub-pages/Comments";
+
+export const HomePage = () => {
+  const [postReference, setPostReference] = useState("");
+  const [postData, setPostData] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("time", "asc")
+      .onSnapshot((snapshot) => {
+        setPostData(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+  }, []);
+
+  const setDefaultPost = (reference) => {
+    setPostReference(reference);
+  };
   return (
-      <div className='homeDiv'>
-          <Followed />
-          <Body />
-          <Comments />
+    <div className="homeDiv">
+      <Followed />
+      <Body
+        posts={postData}
+        postReference={postReference}
+        setDefaultPost={setDefaultPost}
+      />
+      <Comments
+        posts={postData}
+        setPostReference={setPostReference}
+        postReference={postReference}
+      />
     </div>
-  )
-}
-
-export default HomePage
+  );
+};
