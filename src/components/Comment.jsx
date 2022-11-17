67f11e51f6c3c3a6ffd8a1b4a893d4f2df730ac5
white3d <odhiambo.edward111@gmail.com>
2022-11-17 12:58:54 +0300

    Add Comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
-import '../styles/Comment.css'
+import React from "react";
+import "../styles/Comment.css";
+import moment from "moment";
+import { auth } from "../firebase";
 
-const Comment = () => {
+export const Comment = ({ time, username, comment }) => {
   return (
-    <div className='commentSection'>
+    <div>
+      <div className="commentSection">
         <p className="hash">[#]</p>
-        <p className='time'>12:24</p>
-        <p className='username'>Edward</p>
-        <p className='comment'>This is great!!</p>  
+        <p className="time">
+          {moment(new Date(time?.toDate()).toUTCString()).fromNow()}
+        </p>
+        <p className="username">
+          <span
+            style={{
+              color: "red",
+              fontWeight: "bold",
+            }}
+          >
+            {">"}
+          </span>
+          <span
+            style={{
+              color: "green",
+              fontWeight: "bold",
+            }}
+          >
+            {">"}
+          </span>
+          <span
+            style={{
+              color: "yellow",
+              fontWeight: "bold",
+            }}
+          >
+            {">"}
+          </span>{" "}
+          {username === auth.currentUser.displayName ? "You" : username}
+        </p>
+      </div>
+      <div className="individualComment">
+        <p className="comment">{comment}</p>
+      </div>
     </div>
-  )
-}
-
-export default Comment
+  );
+};
