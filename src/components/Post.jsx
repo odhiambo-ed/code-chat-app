618939246a1c1eb909677d15543133399b62d3ac
white3d <odhiambo.edward111@gmail.com>
2022-11-17 13:00:04 +0300

    Add Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,59 +1,109 @@
-import React from "react";
-import Like from "../assets/like.jpeg";
-import Dislike from "../assets/dislike.jpeg";
-import Comments from "../assets/comments.jpeg";
-import Download from "../assets/download.jpeg";
-import User from "../assets/user.jpeg";
+import React, { useState, useEffect } from "react";
 import "../styles/Post.css";
+import User from "../assets/user.jpeg";
+import Comment from "../assets/comments.jpeg";
+import Download from "../assets/download.jpeg";
+import moment from "moment";
+import db, { auth } from "../firebase";
 
-const Post = () => {
+export const Post = ({
+  username,
+  time,
+  body,
+  description,
+  photo,
+  reference,
+  setDefaultPost,
+  postReference,
+}) => {
+  const [comments, setComments] = useState([]);
+  const [followed, setFollowed] = useState([]);
+  useEffect(() => {
+    db.collection(reference).onSnapshot((snapshot) => {
+      setComments(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+    let dbCol = "followed" + auth.currentUser.email;
+    db.collection(dbCol)
+      .where("username", "==", username)
+      .onSnapshot((snapshot) => {
+        setFollowed(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+  }, []);
+  const addFollowed = () => {
+    let dbCol = "followed" + auth.currentUser.email;
+    db.collection(dbCol).add({
+      username: username,
+    });
+  };
   return (
-    <div className="postDiv">
-      <img src={User} alt="User" className="userProfile" />
+    <div
+      className="postDiv"
+      style={{
+        border: postReference === reference && "1px solid orangered",
+      }}
+    >
+      <img className="userProfile" src={User} alt="User" />
       <div className="mainDiv">
+        {/* Name and Date section */}
         <div className="nameSection">
-          <p>Edward</p>
-          <h3>23rd Oct 2022</h3>
-          <button className="followButton">Follow</button>
+          <p>{username === auth.currentUser.displayName ? "You" : username}</p>
+          <h3>{moment(new Date(time?.toDate()).toUTCString()).fromNow()}</h3>
+          {username !== auth.currentUser.displayName && (
+            <>
+              {followed.length > 0 ? (
+                <h3
+                  style={{
+                    marginLeft: "20px",
+                  }}
+                >
+                  Following
+                </h3>
+              ) : (
+                <button onClick={addFollowed} className="followButton">
+                  Follow
+                </button>
+              )}
+            </>
+          )}
         </div>
+        {/* Post Title */}
         <div className="postTitle">
-          <h2 className="postTitleText">JavaScript Meeting Chat</h2>
+          <h2 className="postTitleText">{body}</h2>
         </div>
+        {/* Post Body */}
         <div className="postBody">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-            accusamus molestiae esse doloribus illo ipsum exercitationem quis
-            ipsa consequatur! Consequatur quam animi dolorem, atque repellendus
-            molestiae accusantium? Accusantium quas corporis quis! Blanditiis
-            tempora quas illo harum maxime minima cupiditate veniam id ad
-            placeat. Error, aliquam nemo fugit voluptate asperiores voluptas.
-          </p>
-          <img
-            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvitess.io%2Fimg%2Fgit-workflow.png&f=1&nofb=1"
-            alt="Post Image"
-            className="postImage"
-          />
+          <p>{description}</p>
+          <img className="postImage" src={photo} alt="post image" />
         </div>
+        {/* Utilities */}
         <div className="utilitiesSection">
           <div className="subUtilities">
-            <img src={Like} alt="Like" />
-            <p>20</p>
+            <img
+              onClick={() => {
+                setDefaultPost(reference);
+              }}
+              src={Comment}
+              alt="comment"
+            />
+            <p>{comments.length}</p>
           </div>
           <div className="subUtilities">
-            <img src={Dislike} alt="Dislike" />
-            <p>24</p>
-          </div>
-          <div className="subUtilities">
-            <img src={Comments} alt="Comments" />
-            <p>12</p>
-          </div>
-          <div className="subUtilities">
-            <img src={Download} alt="Download" />
+            <a href={photo} download>
+              <img src={Download} alt="download" />
+            </a>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
-export default Post;
