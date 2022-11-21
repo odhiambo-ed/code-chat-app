fd74574d552f13cc7ad64abfcb71aa017dcbb856
white3d <odhiambo.edward111@gmail.com>
2022-11-21 15:05:07 +0300

    Add Test

---
+++ src/Followed.test.js
@@ -0,0 +1,22 @@
+import { render, screen, cleanup } from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+import FollowedProfiles from './components/FollowedProfiles';
+afterEach(() => {
+    cleanup();
+});
+
+describe('Button Component', () => {
+    render(<FollowedProfiles />);
+    const image = screen.getByAltText('Followed Profile');
+
+    // Test 1
+    test('Image Rendering', () => {
+        expect(image).toBeInTheDocument();
+    });
+
+    // Test 2
+    test('Image Class', () => {
+        expect(image).toHaveClass('userImage');
+    });
+});
