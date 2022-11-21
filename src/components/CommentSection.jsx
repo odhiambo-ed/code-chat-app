ba0e206b495b88d884a99b222ee67d410eb94616
white3d <odhiambo.edward111@gmail.com>
2022-11-21 15:09:36 +0300

    Add Comment Section

---
+++ src/components/CommentSection.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "../styles/Comment.css";
+import moment from "moment";
+import PropTypes from "prop-types";
+import { auth } from "../firebase";
+
+const CommentSection = ({ time, username, comment }) => (
+  <div
+    style={{
+      width: "100%",
+    }}
+  >
+    <div className="commentSection">
+      <p className="hash">[#]</p>
+      <p className="time">
+        {moment(new Date(time?.toDate()).toUTCString()).fromNow()}
+      </p>
+      <p className="username">
+        <span
+          style={{
+            color: "red",
+            fontWeight: "bold",
+          }}
+        >
+          {">"}
+        </span>
+        <span
+          style={{
+            color: "green",
+            fontWeight: "bold",
+          }}
+        >
+          {">"}
+        </span>
+        <span
+          style={{
+            color: "yellow",
+            fontWeight: "bold",
+          }}
+        >
+          {">"}
+        </span>{" "}
+        {username === auth.currentUser.displayName ? "You" : username}
+      </p>
+    </div>
+    <div className="individualComment">
+      <p className="comment">{comment}</p>
+    </div>
+  </div>
+);
+
+CommentSection.propTypes = {
+  time: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  comment: PropTypes.string.isRequired,
+};
+
+export default CommentSection;
