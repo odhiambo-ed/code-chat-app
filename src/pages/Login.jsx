8b5164a7571dc82096c72ca30a27a9220f3eacb7
white3d <odhiambo.edward111@gmail.com>
2022-11-21 15:16:34 +0300

    Edit Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,69 @@
-import React from "react";
-import "../styles/Login.css";
-import GoogleLogo from "../assets/google.jpeg";
-import { auth, provider } from "../firebase";
+import React, { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import db, { auth, provider } from "../firebase";
 
-export const Login = () => {
-  const login = () => {
-    auth.signInWithPopup(provider);
-  };
+import Landing from "../components/Landing";
+
+const Login = () => {
+  const [postData, setPostData] = useState([]);
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("time", "asc")
+      .onSnapshot((snapshot) => {
+        setPostData(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+  }, []);
   return (
-    <div className="loginDiv">
-      <h2 className="appTitle">Code Labs</h2>
-      <div className="signInDiv">
-        <img className="signInImage" src={GoogleLogo} alt="Google Logo" />
-        <p className="signInButton" onClick={login}>
-          Sign in with <span className="signInAlt">Google</span>
-        </p>
-      </div>
-    </div>
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Code-Labs</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto" />
+            <Nav>
+              <Button
+                onClick={() => {
+                  auth.signInWithPopup(provider);
+                }}
+                variant="success"
+              >
+                Sign In
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col md={3} />
+          <Col md={6}>
+            {postData.map((item) => (
+              <Landing
+                key={item.id}
+                username={item.data.username}
+                time={item.data.time}
+                body={item.data.postBody}
+                description={item.data.postDescription}
+                photo={item.data.photo}
+              />
+            ))}
+          </Col>
+          <Col md={3} />
+        </Row>
+      </Container>
+    </>
   );
 };
+
+export default Login;
